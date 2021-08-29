fac0bbfa2069695dc9e70afef938b143e035ff92
‘chenhao’ <chenhao1914@gmail.com>
2021-08-29 22:30:03 +0800

    抽取基类

---
+++ app/src/main/java/com/cc/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cc.myapplication;
+
+import com.cc.baselibrary.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+    }
+}
---
+++ app/src/main/java/com/cc/myapplication/TestFragment.java
@@ -0,0 +1,23 @@
+package com.cc.myapplication;
+
+import android.os.Bundle;
+
+import com.cc.baselibrary.base.BaseFragment;
+
+public class TestFragment extends BaseFragment {
+
+    @Override
+    protected int getContentLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData(Bundle arguments) {
+        super.initData(arguments);
+    }
+}
